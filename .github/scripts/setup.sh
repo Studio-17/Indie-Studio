cad60ba5c3cdecb3fc7c37e6eaf1bc27c88fafd2
Martin Vanaud <martin.vanaud@epitech.eu>
2022-05-09 09:41:21 +0200

    feat(ci): added setup.sh script
    in .github/scripts/, for organization and preparation of the repository

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 chmod +x .github/scripts/*.sh
-ln -sf .github/scripts/pre-push .git/hooks/pre-push
+rm -f .git/hooks/pre-push.sample
+cp .github/scripts/pre-push .git/hooks/pre-push
+ls -l .git/hooks/
 
 echo "[WARNING] ! Do not forgot to modify '.github/workflows/main.yml' with your repository name at line 66"
