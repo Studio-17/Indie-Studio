dc994866b62c031558d7b19aa68466c1f4522509
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-05-12 09:12:49 +0200

    refactor(raylib): removed Camera.hpp/.cpp || feat(raylib): added Window encapsulation functions
    function provided in these files were invalid encapsulation functions (just called raylib equivalent function)
    
    --------------------------------------------------------------------------------------------------------------
    
    In Source/Raylib/Core/Window added Window .cpp/.hpp to encapsulate some Raylib C functions in Raylib::Window class:
        - added Constructor calling raylib InitWindow and handled if window is ready calling IsWindowReady raylib function
        - added Destructor calling raylib CloseWindow function
        - added shouldClose calling raylib ShouldClose function
        - added setWindowSize calling raylib SetWindowSize function
        - added getWindowSize calling raylib GetWindowSize function
        - added setFps calling raylib SetTargetFPS function
        - added getFps calling raylib GetFPS function
        - added showcursor calling raylib ShowCursor function
        - added hideCursor calling raylib HideCursor function
        - added isCursorHidden calling raylib IsCursorHidden function
        - added enableCursor calling raylib EnableCursor function
        - added disableCursor calling raylib DisableCursor function
        - added isCursorOnScreen calling raylib IsCursorOnScreen function
    In Source/Errors/modules/Raylib added WindowError .cpp/.hpp to add a specific error class for Raylib window errors (Error::WindowError)
        this class inherit from Error::Errors class

---
+++ project/Source/Errors/modules/Raylib/WindowError.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** B-YEP-400-PAR-4-1-indiestudio-martin.vanaud
+** File description:
+** WindowError
+*/
+
+#ifndef WINDOWERROR_HPP_
+    #define WINDOWERROR_HPP_
+
+    #include "Errors.hpp"
+
+namespace Error {
+    class WindowError : public Error::Errors {
+        public:
+            WindowError(std::ostream &os, std::string const &message) throw();
+            virtual ~WindowError() throw();
+    };
+}
+
+#endif /* !WINDOWERROR_HPP_ */
