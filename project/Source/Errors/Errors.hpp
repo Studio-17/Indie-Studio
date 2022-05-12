d220b3919218650a8314490aeda9063e434e742f
Gurvan Le Letty <gurvan.le-letty@epitech.eu>
2022-05-12 09:40:50 +0200

    style(error): added Errors class inside a Error namespace
    In Source/Errors/ changed Errors .cpp/.hpp

--- project/Source/Errors/Errors.hpp
+++ project/Source/Errors/Errors.hpp
@@ -6,22 +6,24 @@
 */
 
 #ifndef ERRORS_HPP_
-#define ERRORS_HPP_
+    #define ERRORS_HPP_
 
-#include <exception>
-#include <iostream>
-#include <string>
+    #include <exception>
+    #include <iostream>
+    #include <string>
 
-class Errors : public std::exception
-{
-    public:
-        Errors(std::ostream &os, const std::string &message) throw();
-        virtual ~Errors() throw();
-        const char *what() const throw();
+namespace Error {
+    class Errors : public std::exception
+    {
+        public:
+            Errors(std::ostream &os, std::string const &message) throw();
+            virtual ~Errors() throw();
+            const char *what() const throw();
 
-    private:
-        std::string _message;
-        std::ostream &_os;
-};
+        private:
+            std::string _message;
+            std::ostream &_os;
+    };
+}
 
 #endif /* !ERRORS_HPP_ */
