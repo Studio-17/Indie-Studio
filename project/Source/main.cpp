24eab475455c459bbe20b956d1c0885b8edde79f
Martin Vanaud <martin.vanaud@epitech.eu>
2022-05-25 10:48:00 +0200

    fix(conflicts) resolved conflits with master
    in Source/Map/, removed header and soure code unused

--- project/Source/main.cpp
+++ project/Source/main.cpp
@@ -6,65 +6,12 @@
 */
 
 #include "Core.hpp"
-
-#include "Objects/MapObject/Map/Map.hpp"
-#include "Objects/MapObject/Block/Block.hpp"
+#include "Core.hpp"
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    Core core;
 
-    InitWindow(screenWidth, screenHeight, "Indie Studio");
-
-    // Define the camera to look into our 3d world
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 300.0f, 0.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 0.0f, 90.0f };
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    SetCameraMode(camera, CAMERA_FREE); // Set a free camera mode
-
-    Object::Map map("Assets/Maps/map.txt");
-    map.process();
-
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);          // Update camera
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-            ClearBackground(DARKGRAY);
-
-            BeginMode3D(camera);
-
-                DrawLine3D((Vector3){-100, 0, 0}, (Vector3){100, 0, 0}, GREEN);     // X
-                DrawLine3D((Vector3){0, -100, 0}, (Vector3){0, 100, 0}, RED);       // Y
-                DrawLine3D((Vector3){0, 0, -100}, (Vector3){0, 0, 100}, DARKBLUE);  // Z
-
-                map.draw();
-
-            EndMode3D();
-
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
+    core.loop();
     return 0;
 }
