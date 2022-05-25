704fe08c177abf679aecb26b90188b75bb499c71
Victor PALLE <victor.palle@epitech.eu>
2022-05-25 19:08:35 +0200

    feat(scene): fix back button

--- project/Source/Scenes/NonGameScenes/PopScenes/SettingsScene.cpp
+++ project/Source/Scenes/NonGameScenes/PopScenes/SettingsScene.cpp
@@ -22,11 +22,16 @@ void Scene::SettingsScene::newGameScene(void)
     _nextScene = Scene::Scenes::GAME;
 }
 
+void Scene::SettingsScene::mainMenuScene(void)
+{
+    _nextScene = Scene::Scenes::MAIN_MENU;
+}
+
 Scene::SettingsScene::SettingsScene(std::shared_ptr<Settings> settings) : AScene(settings)
 {
     // _buttons.emplace(Scene::BUTTONSNAME::NEWGAME, std::make_unique<Object::Button>("Save/button.png", 3, std::bind(&Scene::SettingsScene::newGameScene, this), "Save/assets_sound_Click.ogg", Position(700, 300, 0)));
     // _buttons.emplace(Scene::BUTTONSNAME::EXIT, std::make_unique<Object::Button>("Save/button.png", 3, std::bind(&Scene::SettingsScene::exitScene, this),"Save/assets_sound_Click.ogg", Position(700, 500, 0)));
-    _buttons.emplace(Scene::BUTTONSNAME::SETTINGS, std::make_unique<Object::Button>("Save/button.png", 3, std::bind(&Scene::SettingsScene::settingsScene, this),"Save/assets_sound_Click.ogg", Position(700, 800, 0)));
+    _buttons.emplace(Scene::BUTTONSNAME::MAIN_MENU, std::make_unique<Object::Button>("Save/button.png", 3, std::bind(&Scene::SettingsScene::mainMenuScene, this),"Save/assets_sound_Click.ogg", Position(700, 800, 0)));
     _nextScene = Scene::Scenes::SETTINGS;
 
     // MUSIC HANDLING
@@ -46,6 +51,7 @@ void Scene::SettingsScene::fadeBlack()
 
 Scene::Scenes Scene::SettingsScene::handelEvent()
 {
+    _nextScene = Scene::Scenes::SETTINGS;
     for (auto &[type, button] : _buttons)
         button->checkHover(GetMousePosition());
     return _nextScene;
