9521946efe642a169b2964f4b32d14162f202f62
Maxime Carabina <maxime.carabina@epitech.eu>
2022-05-31 13:30:25 +0200

    ci(menuScenes): added scene SelectGameMenu - in Source/Core/Core.cpp, added scene SelectGameMenu in _menuScenes map

--- project/Source/Core/Core.cpp
+++ project/Source/Core/Core.cpp
@@ -7,6 +7,7 @@
 
 #include "MainMenuScene.hpp"
 #include "SettingsScene.hpp"
+#include "SelectGameMenuScene.hpp"
 
 #include "tools.hpp"
 #include "Map.hpp"
@@ -30,6 +31,7 @@ void Core::loadMenuScenes()
 {
     _menuScenes.emplace(Scene::Scenes::MAIN_MENU, std::make_shared<Scene::MainMenuScene>(_settings));
     _menuScenes.emplace(Scene::Scenes::SETTINGS, std::make_shared<Scene::SettingsScene>(_settings));
+    _menuScenes.emplace(Scene::Scenes::GAME, std::make_shared<Scene::SelectGameMenuScene>(_settings));
     //rajouter toutes les scènes des menus
 }
 
