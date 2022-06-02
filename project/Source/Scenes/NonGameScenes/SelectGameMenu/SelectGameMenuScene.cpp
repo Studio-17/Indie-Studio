e7022be553ebdf65767660558dd5c33bf702b131
Maxime Carabina <maxime.carabina@epitech.eu>
2022-06-02 15:09:14 +0200

    refactor(parallax): moved parallax function
    - in SelectGameMenuScene.cpp, parallax function was moved in function handleEvent

--- project/Source/Scenes/NonGameScenes/SelectGameMenu/SelectGameMenuScene.cpp
+++ project/Source/Scenes/NonGameScenes/SelectGameMenu/SelectGameMenuScene.cpp
@@ -37,6 +37,7 @@ Scene::SelectGameMenuScene::SelectGameMenuScene(std::shared_ptr<Settings> settin
     }
     _images = loadObjects<Object::Image>("Conf/Scenes/SelectGameMenu/image.json");
     _texts = loadObjects<Object::Text>("Conf/Scenes/SelectGameMenu/text.json");
+    _parallax = loadObjects<Object::Image>("Conf/Scenes/parallax.json");
 
     _nextScene = Scene::Scenes::GAME;
 }
@@ -51,6 +52,19 @@ void Scene::SelectGameMenuScene::fadeBlack()
 
 Scene::Scenes Scene::SelectGameMenuScene::handelEvent()
 {
+    std::float_t speed = 0.0;
+    int index = 0;
+
+    for (auto &parallax : _parallax) {
+        if (index % 2 == 0)
+            speed += 0.15;
+
+        parallax->setPosition(parallax->getPosition().getX() - speed, parallax->getPosition().getY());
+        if (parallax->getPosition().getX() <= -1930)
+            parallax->setPosition(1928, parallax->getPosition().getY());
+        index++;
+    }
+
     _nextScene = Scene::Scenes::GAME;
 
     for (auto &button : _buttons)
@@ -60,18 +74,8 @@ Scene::Scenes Scene::SelectGameMenuScene::handelEvent()
 
 void Scene::SelectGameMenuScene::draw()
 {
-    std::float_t speed = 0.0;
-
-    for (std::int8_t i = 0; i < 10; i++) {
-        if (i % 2 == 0)
-            speed += 0.1;
-
-        _images.at(i)->setPosition(_images.at(i)->getPosition().getX() - speed, _images.at(i)->getPosition().getY());
-
-        if (_images.at(i)->getPosition().getX() <= -1930)
-            _images.at(i)->setPosition(1928, _images.at(i)->getPosition().getY());
-    }
-
+    for (auto &parallax : _parallax)
+        parallax->draw();
     for (auto &image : _images)
         image->draw();
     for (auto &text : _texts)
