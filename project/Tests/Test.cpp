a2aa17ef6ab8900742aa21e915b3a578d5e15295
Martin Vanaud <martin.vanaud@epitech.eu>
2022-06-09 21:47:45 +0200

    feat(testing) added simple test on createColor function from tools

--- project/Tests/Test.cpp
+++ project/Tests/Test.cpp
@@ -5,19 +5,21 @@
 ** Test
 */
 
-#include <criterion/criterion.h>
+#include "raylib.h"
+
 #include <criterion/assert.h>
+#include <criterion/criterion.h>
 #include <criterion/redirect.h>
 
-#include "Tools/clock.hpp"
+#include "Tools/tools.hpp"
 
-Test(Tools_Clock, clockTest)
+Test(Tools_CreateColor, toolsTest)
 {
-    Clock clock;
+    std::array<float, 4> const &array = {40.5f, 50.66f, 10.0f, 0.0f};
+    Color color = createColor(array);
 
-    clock.start();
-
-    clock.getElapsedTime();
-
-    cr_assert(false);
+    cr_assert_float_eq(color.r, 40.5, 40.5);
+    cr_assert_float_eq(color.g, 50.66, 50.66);
+    cr_assert_float_eq(color.b, 10.0, 10.5);
+    cr_assert_float_eq(color.a, 0.0, 0.0);
 }
