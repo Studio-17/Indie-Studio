d1714b3abe3fd5e35b709fa3f3ebe177e510ccf1
Martin Vanaud <martin.vanaud@epitech.eu>
2022-06-19 15:19:38 +0200

    documentation(doxygen) added documentation for Core module

--- project/Source/Core/Core.hpp
+++ project/Source/Core/Core.hpp
@@ -20,39 +20,80 @@
     #include "Image.hpp"
     #include "Text.hpp"
 
+/**
+ * @brief 
+ * 
+ */
 class Core {
     public:
+        /**
+         * @brief Construct a new Core object
+         */
         Core();
+        /**
+         * @brief Destroy the Core object
+         */
         ~Core();
 
+        /**
+         * @brief Handle loading of the menu scenes
+         */
         void loadMenuScenes();
+        /**
+         * @brief Core loop that handles the game loop
+$         */
         void loop();
 
+        /**
+         * @brief Get the Event object, and handle response
+$         */
         void getEvent();
+        /**
+         * @brief Bind the keyboard and gamepad events
+         * 
+         * @param player 
+         * @param action 
+         * @param Key 
+         */
         void bindKey(int player, int action, int Key);
 
+        /**
+         * @brief Load keys bindings and apply them
+         * 
+         * @param jsonData 
+         */
         void loadKeyBinding(nlohmann::json const &jsonData);
 
+        /**
+         * @brief Core function to handle the waiting sequence
+         * 
+         */
         void waitingLoad();
 
+        /**
+         * @brief Handle loading screen
+         *
+         * @param string
+         */
         void updateLoadingScreen(std::string const &string);
+
     protected:
     private:
-        std::shared_ptr<Settings> _settings;
-        std::shared_ptr<GameSettings> _gameSettings;
-        bool _isRunning;
-        std::unordered_map<Scene::Scenes, std::shared_ptr<Scene::IScene>> _menuScenes;
-        Scene::Scenes _activeScene;
-        Keyboard _keyboard;
-        Gamepad _gamepad;
+        std::shared_ptr<Settings> _settings; ///< Settings object
+        std::shared_ptr<GameSettings> _gameSettings; ///< Game settings object
+        bool _isRunning; ///< Is running
+        std::unordered_map<Scene::Scenes, std::shared_ptr<Scene::IScene>> _menuScenes; ///< Menu scenes
+        Scene::Scenes _activeScene; ///< Active scene
+        Keyboard _keyboard; ///< Keyboard object
+        Gamepad _gamepad; ///< Gamepad object
 
-        std::map<Action, int> _actionPressed;
-        std::vector<std::map<PlayerAction, int>> _playerActions;
-        std::vector<std::pair<PlayerAction, int>> _gamepadPlayerActions;
-        std::map<PlayerAction, int> _gamepadPlayerMovement;
+        std::map<Action, int> _actionPressed; ///< Action pressed
+        std::vector<std::map<PlayerAction, int>> _playerActions; ///< Player actions
+        std::vector<std::pair<PlayerAction, int>> _gamepadPlayerActions; ///< Gamepad player actions
+        std::map<PlayerAction, int> _gamepadPlayerMovement; ///< Gamepad player movement
 
-        std::vector<std::unique_ptr<Object::Image>> _images;
-        std::vector<std::unique_ptr<Object::Text>> _texts;
+        std::vector<std::unique_ptr<Object::Image>> _images; ///< Images
+        std::vector<std::unique_ptr<Object::Text>> _texts; ///< Texts
 
         std::vector<std::unique_ptr<Object::Image>> _parallax; ///< Vector of every Images of the parallax
 };
