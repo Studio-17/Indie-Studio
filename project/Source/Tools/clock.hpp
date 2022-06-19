60a70fbe3de44decdf03a0bbf0a8e4de26bd2e98
Clement Fernandes <clement.fernandes@epitech.eu>
2022-06-19 11:32:14 +0200

    fix(bombs): fixed the explosion with the bomb depending on the clock

--- project/Source/Tools/clock.hpp
+++ project/Source/Tools/clock.hpp
@@ -69,24 +69,23 @@ using timePoint = std::chrono::time_point<std::chrono::system_clock>;
         void reset()
         {
             _start = std::chrono::system_clock::now();
+            _end = std::chrono::system_clock::now();
+            _startPause = std::chrono::system_clock::now();
+            _endPause = std::chrono::system_clock::now();
             _isPaused = false;
             _elapsedTime = 0;
             _elapsedTimePause = 0;
             _tmpElapsedTime = 0;
-            _start = std::chrono::system_clock::now();
-            _end = std::chrono::system_clock::now();
-            _startPause = std::chrono::system_clock::now();
-            _endPause = std::chrono::system_clock::now();
         }
 
     protected:
     private:
-        std::chrono::time_point<std::chrono::system_clock> _start;
-        std::chrono::time_point<std::chrono::system_clock> _end;
-        std::chrono::time_point<std::chrono::system_clock> _startPause;
-        std::chrono::time_point<std::chrono::system_clock> _endPause;
+        timePoint _start;
+        timePoint _end;
+        timePoint _startPause;
+        timePoint _endPause;
         long long int _elapsedTimePause = 0;
-        long long int _elapsedTime;
+        long long int _elapsedTime = 0;
         long long int _tmpElapsedTime = 0;
         bool _isPaused = false;
     };
